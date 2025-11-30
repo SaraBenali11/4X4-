39a093e086c25003ad828f8a5395f104b5ed322b
Sara Benali <sara.benali@ensia.edu.dz>
2025-11-30 22:03:07 +0100

    Add pages folder with React components

---
+++ src/frontend/views/pages/loginpage.jsx
@@ -0,0 +1,20 @@
+import Footer from '../components/footer';
+import Header from '../components/header';
+import AdminLogin from '../components/AdminLogin';
+/**
+ * Homepage component
+ * Main landing page with carousel, new products, and reviews
+ */
+function LoginPage() {
+  return (
+    <div className="homepage">
+      <Header />
+      <main>
+        <AdminLogin />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default LoginPage;
