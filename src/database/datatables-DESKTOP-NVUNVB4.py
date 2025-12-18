ac6e62f89adebcf1139b3ab98b5acde81b716a35
kahina <laperlenoire2023@gmail.com>
2025-12-18 04:00:03 +0100

    chore: Clean admin branch for restructuring

--- src/database/datatables-DESKTOP-NVUNVB4.py
+++
@@ -1,2 +0,0 @@
-# Backwards-compatible copy (desktop-specific) left here to preserve history
-from .datatables import *  # noqa: F401,F403
--- src/frontend/views/components/filteringbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FilterBar(){
-    return <div>
-        <div className="filter-container">
-            
-        </div>
-    </div>
-}
